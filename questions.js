ea9c9f5f0c98f45258a040c0f253c30315700a44
holson1 <henryolson1@gmail.com>
2014-02-21 22:58:55 -0800

    version 1.0

---
+++ questions.js
@@ -0,0 +1,71 @@
+//Henry Olson
+//W0755449
+
+var geoQuestions = [];
+geoQuestions['Coos Bay'] = {
+	q: "Which famous University of Oregon trackstar was born in Coos Bay?",
+	choices: ["Usain Bolt", "Edwin Moses", "Steve Prefontaine", "Bob Hayes"],
+	correctAns: "Steve Prefontaine"	
+};
+
+geoQuestions['Portland'] = {
+	q: "Which of the following is NOT a nickname of Portland?",
+	choices: ["City of Roses", "Rain City", "Stumptown", "Rip City"],
+	correctAns: "Rain City"	
+};
+
+geoQuestions['Ashland'] = {
+	q: "What annual festival is Ashland best known for?",
+	choices: ["Dog Days", "Southern Oregon Beer Festival", "Shakespeare Festival", "Lithia Fest"],
+	correctAns: "Shakespeare Festival"	
+};
+
+geoQuestions['Malheur National Forest'] = {
+	q: "Malheur National Forest is home to the Earth's largest organism. What is it?",
+	choices: ["An aspen grove", "A douglas fir", "A grove of wildflowers", "A mushroom"],
+	correctAns: "A mushroom"	
+};
+
+geoQuestions['Crater Lake'] = {
+	q: "Crater Lake is famous for being the _______ lake in America.",
+	choices: ["Largest", "Deepest", "Murkiest", "Coldest"],
+	correctAns: "Deepest"	
+};
+
+geoQuestions['Astoria'] = {
+	q: "Which popular movie was filmed in Astoria?",
+	choices: ["Stand By Me", "The Hunt for Red October", "Twilight", "The Goonies"],
+	correctAns: "The Goonies"	
+};
+
+geoQuestions['Hood River'] = {
+	q: "Hood River and the Columbia River Gorge offer many attractions to visitors. Which of the following is NOT one?",
+	choices: ["Orchards", "Windsurfing", "Skiing", "Wineries"],
+	correctAns: "Skiing"
+};
+
+geoQuestions['Bend'] = {
+	q: "_______ is the largest microbrewery in Bend and the 5th largest in the nation. (Hint: Mirror Pond Pale Ale)",
+	choices: ["Deschutes Brewery", "Western Brewing Co.", "Budweiser", "Laurelwood Brewing Co."],
+	correctAns: "Deschutes Brewery"
+};
+
+geoQuestions['Mt. Hood'] = {
+	q: "Which famous horror movie was filmed at Mt. Hood's Timberline Lodge?",
+	choices: ["Silence of the Lambs", "The Ring", "Saw", "The Shining"],
+	correctAns: "The Shining"
+};
+
+geoQuestions['Eugene'] = {
+	q: "What is the name of the sports team at the university in Eugene?",
+	choices: ["Timbers", "Ducks", "Lumberjacks", "Beavers"],
+	correctAns: "Ducks"
+};
+
+geoQuestions['Portland International Airport'] = {
+	q: "What is the 3 letter code for Portland's Airport?",
+	choices: ["POR", "PTL", "PDX", "PTD"],
+	correctAns: "PDX"
+};
+
+
